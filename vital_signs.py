f82a8f728d23084ce73f16800e1fe00116e0584d
KasperJosh <visario.joshua@gmail.com>
2026-01-29 19:35:07 -0500

    New Vital Signs Document

---
+++ vital_signs.py
@@ -0,0 +1,18 @@
+#Code that records the vital signs of pts
+
+print("Welcome to the Vital Signs Recorder")
+print("Please enter all the required information on the patients")
+
+
+
+patient_name = str(input("Please enter the patient's name: "))
+patient_MRN = int(input("Please enter the patient's MRN: "))
+patient_BP = str(input("Please enter the patient's blood pressure: "))
+patient_HR = int(input("PLease enter the patient's HR: "))
+patient_spO2 = int(input("Please enter the patient's oxygen saturation: "))
+patient_rr = int(input("Please enter the patient's respiratory rate: "))
+patient_temp = float(input("Please enter the patient's temperature: "))
+
+
+
+
