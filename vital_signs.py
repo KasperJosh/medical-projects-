24a38733794d9ff8963cb2d802b8422a03d5c2f2
KasperJosh <visario.joshua@gmail.com>
2026-01-29 20:07:02 -0500

    Saving V/S

--- vital_signs.py
+++ vital_signs.py
@@ -1,6 +1,6 @@
 #Code that records the vital signs of pts
 
-print("Welcome to the Vital Signs Recorder")
+print("Welcome to the Vital Signs Recorder!")
 print("Please enter all the required information on the patients")
 
 
@@ -8,7 +8,7 @@ print("Please enter all the required information on the patients")
 patient_name = str(input("Please enter the patient's name: "))
 patient_MRN = int(input("Please enter the patient's MRN: "))
 patient_BP = str(input("Please enter the patient's blood pressure: "))
-patient_HR = int(input("PLease enter the patient's HR: "))
+patient_HR = int(input("PLease enter the patient's pulse rate: "))
 patient_spO2 = int(input("Please enter the patient's oxygen saturation: "))
 patient_rr = int(input("Please enter the patient's respiratory rate: "))
 patient_temp = float(input("Please enter the patient's temperature: "))
