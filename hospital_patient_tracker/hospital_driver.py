443316bb7d6965c76814ed37561f7b5de8ad9f03
KasperJosh <visario.joshua@gmail.com>
2026-04-07 17:45:21 -0400

    Added Admit Patient

--- hospital_patient_tracker/hospital_driver.py
+++ hospital_patient_tracker/hospital_driver.py
@@ -4,7 +4,7 @@
 
 
 #Importing the necessary modules
-import patient
+import patient_info
 
 def welcome():
         print('+++++++++++++++++++++++++++++++++++++++++++')
@@ -29,6 +29,74 @@ def menu():
         else:
             print("Please try again")
               
+# Adding a patient to the unit method
+def admit_patient(patients):
+        
+    room = input("Please enter the admitting room: ")
+    name = input("Please enter the patient's name: ")
+    mrn = int(input("Please enter the patient's MRN: "))
+    age = int(input("Please enter the patient's age: "))
+    gender = input("Please enter the patient's gender: ")
+    admission_date = input("Please enter the admitting date: ")
+    team_doctor = input("Please enter the team of doctor: ")
+    diagnosis = input("Please enter the diagnosis: ")
+    isolation = input("Please enter the isolation status ")
+    level_intervention = input("Please enter the level of intervention ")
+    past_hx = input("Please enter the past medical history ")
+    allergies = input("Please enter the allergies: ")
+    type_sx = input("Please enter the type of surgery: ")
+    procedures =input("Please enter the procedures done: ")
+    rhythm= input("Please enter the cardiac rhythm: ")
+    ventilation= input("Please enter the ventilation status: ")
+    iv_access= input("Please enter the IV accesses: ")
+    nutrition= input("Please enter the nutrition status: ")
+    dressings= input("Please enter the dressings present: ")
+    elimination= input("Please enter the elimination status ")
+    mobility= input("Please enter the mobility status: ")
+    labs = input("Please enter the critical labs")
+    medications = input("Please enter the medication list ")
+    issues= input("Please enter the current issues: ")
+    plans= input("Please enter the plan: ")
+    pros_involved= input("Please enter the pros involved: ")
+    home_screen= input("Please enter the home situation: ")
+    possible_dc= input("Please enter the p0ssible D/C date ")
+
+    patient = patient_info.Patient(
+        room,
+        name,
+        mrn,
+        age,
+        gender,
+        admission_date,
+        team_doctor,
+        diagnosis,
+        isolation ,
+        level_intervention,
+        past_hx,
+        allergies,
+        type_sx,
+        procedures,
+        rhythm,
+        ventilation,
+        iv_access,
+        nutrition,
+        dressings,
+        elimination,
+        mobility,
+        labs,
+        medications,
+        issues,
+        plans,
+        pros_involved,
+        home_screen,
+        possible_dc
+    )
+    
+    patients[mrn] = patient
+    #To test
+    patient.display_info()
+    print("Patient successfully added!")
+
 
 class Hospital_Driver:
     
@@ -49,7 +117,8 @@ class Hospital_Driver:
             # Adding a patient to the unit
             case 1:  
                 print("Admit a patient")
-                admit_patient()
+                admit_patient(patients)
+                print(patients)
                 break
             # Discharging patient from unit
             case 2:
@@ -78,36 +147,4 @@ class Hospital_Driver:
             print("Thank you for visiting the unit! Application now closing")
     
 
-    # Adding a patient to the unit method
-
-    def admit_patient():
-        
-        room = input("Please enter the admitting room: ")
-        name = input("Please enter the patient's name: ")
-        mrn = input("Please enter the patient's MRN: ")
-        age = input("Please enter the patient's age: ")
-        gender = input("Please enter the patient's gender: ")
-        admission_date = input("Please enter the admitting date: ")
-        team_doctor = input("Please enter the team of doctor: ")
-        diagnosis = input("Please enter the diagnosis: ")
-        
-        isolation = input("Please enter the isolation status ")
-        level_intervention = input("Please enter the level of intervention ")
-        past_hx = input("Please enter the past medical history "),
-        allergies = input("Please enter the allergies: "),
-        type_sx = input("Please enter the type of surgery: "),
-        procedures =input("Please enter the procedures done: "),
-        rhythm= input("Please enter the cardiac rhythm: "),
-        ventilation= input("Please enter the ventilation status: "),
-        iv_access= input("Please enter the IV accesses: "),
-        nutrition= input("Please enter the nutrition status: "),
-        dressings= input("Please enter the dressings present: "),
-        elimination= input("Please enter the elimination status "),
-        mobility= input("Please enter the mobility status: "),
-        labs = input("Please enter the critical labs"),
-        medications = input("Please enter the medication list "),
-        issues= input("Please enter the current issues: "),
-        plans= input("Please enter the plan: "),
-        pros_involved= input("Please enter the pros involved: "),
-        home_screen= input("Please enter the home situation: "),
-        possible_dc= input("Please enter the p0ssible D/C date "),
+    
